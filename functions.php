201af7d5ff0bd20ea5ee5a7fc62b030305640e92
alex chousmith <alex@ninthlink.com>
2014-04-22 22:30:40 +0000

    more WORK landing tweak

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 // and then?
 function nl2k14_js() {
 	if(!is_admin()) {
-		wp_enqueue_script( 'nlkjs', get_stylesheet_directory_uri() .'/js/nlk.js', array('jquery'), false, true );
+		wp_enqueue_script( 'nlkjs', get_stylesheet_directory_uri() .'/js/nlk.js', array('jquery'), '0.1', true );
 	}
 }
 add_action( 'wp_print_scripts', 'nl2k14_js' );
--- js/nlk.js
+++ js/nlk.js
@@ -1,3 +1,8 @@
 jQuery(function($) {
-	console.log('and then?');
+	if ( $('#portfolio').size() > 0 ) {
+		$('#portfolio .element').each(function() {
+			$(this).children('.nectar-love-wrap').remove();
+			$(this).find('.work-info a:last').addClass('detaild').siblings().remove();
+		});
+	}
 });
