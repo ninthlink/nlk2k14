567c159d656463d09cff240dfa08a346642243ef
Tim Spinks <tim@ninthlink.com>
2014-04-24 21:27:34 +0000

    update dynamic styles call

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ add_action( 'wp_print_scripts', 'nl2k14_js' );
 
 
 function nl2k14_styles() {	
-		 wp_register_style("scrollbar-style", get_stylesheet_directory_uri() . "/css/scrollbar.php");
-		 wp_enqueue_style('scrollbar-style'); 
+		 wp_register_style("dynamic-styles", get_stylesheet_directory_uri() . "/css/dynamic_styles.php");
+		 wp_enqueue_style('dynamic-styles'); 
 }
 add_action('wp_enqueue_scripts', 'nl2k14_styles');
