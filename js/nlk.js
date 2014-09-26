58e7b0b949a14ccce26d3372fc014de7d53f85c0
Tim Spinks <tim@ninthlink.com>
2014-09-26 16:06:28 +0000

    comment out rollover removing JS

--- js/nlk.js
+++ js/nlk.js
@@ -5,15 +5,19 @@ jQuery(function($) {
 		}
 	});
 
-	if ( $('#portfolio').size() > 0 ) {
-		$('#portfolio .element').each(function() {
-			$(this).children('.nectar-love-wrap').remove();
-			// no more DETAILS rollover, just make it all click
-			var wurl = $(this).find('.work-info a:last').attr('href');
-			$(this).children('.work-item').children('img').siblings().remove();
-			$(this).wrapInner('<a />').children('a').attr('href',wurl);
-		});
+	/*
+	if ( $('div.no-portfolio-rollovers').length ) {
+		if ( $('#portfolio').size() > 0 ) {
+			$('#portfolio .element').each(function() {
+				$(this).children('.nectar-love-wrap').remove();
+				// no more DETAILS rollover, just make it all click
+				var wurl = $(this).find('.work-info a:last').attr('href');
+				$(this).children('.work-item').children('img').siblings().remove();
+				$(this).wrapInner('<a />').children('a').attr('href',wurl);
+			});
+		}
 	}
+	*/
 	
 	if ( $('body').hasClass('single-portfolio') ) {
 		$('.sf-menu li:first, #mobile-menu li:first').addClass('current-menu-item');
